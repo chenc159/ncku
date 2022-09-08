4bf064a1b3cea17f6e5b049fbc6aa5c5f04180eb
chenc159 <chihchun.chen@mail.utoronto.ca>
2022-09-08 22:15:45 +0800

    20220908

--- preplan.py
+++ preplan.py
@@ -1,3 +1,4 @@
+from ast import Lt
 from cmath import sqrt
 import math
 import numpy as np
@@ -384,7 +385,7 @@ def triangle_c(Desired_dist, Radius, Angle, Waypt_num, x, y):
 
         # At each waypt, leader and follower's angle (rad)
         Lang, Fang = Lt1, Ft1
-        # Change of angle, neg if pivot_id=1, pos if pivot_id[i]=2
+        # Change of angle, neg if pivot_id=1, pos if pivot_id[i]=2 (dLang == dFang)
         dLang = math.copysign(1, pivot_id[i]-1.5)*min(abs(Lt2 - Lt1), abs(2*math.pi - abs(Lt2 - Lt1)))/wpt_num 
         dFang = math.copysign(1, pivot_id[i]-1.5)*min(abs(Ft2 - Ft1), abs(2*math.pi - abs(Ft2 - Ft1)))/wpt_num
         cir_lx, cir_ly, cir_ofx, cir_ofy = [], [], [], []
@@ -421,11 +422,254 @@ def triangle_c(Desired_dist, Radius, Angle, Waypt_num, x, y):
     # plt.legend(['wpt','uav1', 'uav2', 'uav3'])
     plt.show()
 
+def dec_speed(v0, a):
+    t = (0-v0)/a
+    s = v0*t + 0.5*a*(t**2)
+    print('dec_speed', round(s, 3))
+    return round(s, 3)
+
+def three_stage_pos(Desired_dist, Angle, x, y, v0, a):
+    '''Three stages: go straight with constant speed, go straight decelerating, and turning'''
+
+    # dist to wpt that shall slow down
+    dec_v_dist = dec_speed(v0, a)
+    
+    # pivot point (add start and final just to balance length)
+    pivot_id, pivot_x, pivot_y = [0], [x[0]], [y[0]]
+    # total turning angle around pivot
+    Ltang = [0]
+    
+    # Calculate angles relative to leader's position
+    ang = {0: 0, 1: Angle*math.pi/180, 2: -Angle*math.pi/180}
+
+    # add leader, follow1, follow2's first coordinate (relative to leader)
+    des_hdg0 = math.atan2((y[1] - y[0]),(x[1] - x[0]))
+    wp_x = {0: [x[0]], 1: [x[0]+Desired_dist*math.cos(math.pi+des_hdg0+ang[1])], 2: [x[0]+Desired_dist*math.cos(math.pi+des_hdg0+ang[2])]}
+    wp_y = {0: [y[0]], 1: [y[0]+Desired_dist*math.sin(math.pi+des_hdg0+ang[1])], 2: [y[0]+Desired_dist*math.sin(math.pi+des_hdg0+ang[2])]}
+
+    # calculate vehicles' coordinates
+    for i in range(1, len(x)-1): # omit first and last point cuz no turning at these points
+        xs, ys = symbols('xs ys')
+        # y = mx + b 
+        m1, m2 = (y[i] - y[i-1])/(x[i] - x[i-1]), (y[i+1] - y[i])/(x[i+1] - x[i]) # calculate slope
+        hdg1 = math.atan2((y[i] - y[i-1]),(x[i] - x[i-1]))
+        # hdg2 = math.atan2((y[i] - y[i+1]),(x[i] - x[i+1]))
+        hdg2 = math.atan2((y[i+1] - y[i]),(x[i+1] - x[i]))
+
+        # Calculate follower1's wpts' path intersection
+        x11, y11 = x[i] + Desired_dist*math.cos(math.pi+hdg1+ang[1]), y[i] + Desired_dist*math.sin(math.pi+hdg1+ang[1])
+        x12, y12 = x[i] + Desired_dist*math.cos(math.pi+hdg2+ang[1]), y[i] + Desired_dist*math.sin(math.pi+hdg2+ang[1])
+        b1 = y11 - m1*x11
+        b2 = y12 - m2*x12
+        eq1 = Eq(ys-m1*xs-b1)
+        eq2 = Eq(ys-m2*xs-b2)
+        sol1 = solve((eq1,eq2), (xs, ys))
+
+        # calculate follower2's wpts' path intersection
+        x21, y21 = x[i] + Desired_dist*math.cos(math.pi+hdg1+ang[2]), y[i] + Desired_dist*math.sin(math.pi+hdg1+ang[2])
+        x22, y22 = x[i] + Desired_dist*math.cos(math.pi+hdg2+ang[2]), y[i] + Desired_dist*math.sin(math.pi+hdg2+ang[2])
+        b1 = y21 - m1*x21
+        b2 = y22 - m2*x22
+        eq1 = Eq(ys-m1*xs-b1)
+        eq2 = Eq(ys-m2*xs-b2)
+        sol2 = solve((eq1,eq2), (xs, ys))
+
+        # Follower with shorter path-to-travel is the pivot
+        if (sol1[xs]-x11)**2+(sol1[ys]-y11)**2 < (sol2[xs]-x21)**2+(sol2[ys]-y21)**2: # turn right, F1 as pivot
+            pivot_id.append(1)
+            pivot_x.append(sol1[xs])
+            pivot_y.append(sol1[ys])
+        else: # turn left, F2 as pivot
+            pivot_id.append(2)
+            pivot_x.append(sol2[xs])
+            pivot_y.append(sol2[ys])
+
+        # leader's position before and after turning
+        Lpx_bt = pivot_x[i] - Desired_dist*math.cos(math.pi+hdg1+ang[pivot_id[i]])
+        Lpy_bt = pivot_y[i] - Desired_dist*math.sin(math.pi+hdg1+ang[pivot_id[i]])
+        Lpx_at = pivot_x[i] - Desired_dist*math.cos(math.pi+hdg2+ang[pivot_id[i]])
+        Lpy_at = pivot_y[i] - Desired_dist*math.sin(math.pi+hdg2+ang[pivot_id[i]])
+
+        # Leader's (= Follower's) turning angle wrt pivot (1 as start, 2 as end, ang as totle ang)
+        Lt1 = math.atan2((pivot_y[i] - Lpy_bt),(pivot_x[i] - Lpx_bt)) + math.pi
+        Lt2 = math.atan2((pivot_y[i] - Lpy_at),(pivot_x[i] - Lpx_at)) + math.pi
+        Ltang.append(math.copysign(1, pivot_id[i]-1.5)*min(abs(Lt2 - Lt1), abs(2*math.pi - abs(Lt2 - Lt1))))
+        
+        # add the leader and followers' locations of starting to slow down
+        wp_x[0].append(round(Lpx_bt-dec_v_dist*math.cos(hdg1),2))
+        wp_y[0].append(round(Lpy_bt-dec_v_dist*math.sin(hdg1),2))
+        wp_x[1].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg1+ang[1]),2))
+        wp_y[1].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg1+ang[1]),2))
+        wp_x[2].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg1+ang[2]),2))
+        wp_y[2].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg1+ang[2]),2))
+
+        # add the leader and followers' locations before turning (shall be v = 0)
+        wp_x[0].append(round(Lpx_bt,2))
+        wp_y[0].append(round(Lpy_bt,2))
+        wp_x[1].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg1+ang[1]),2))
+        wp_y[1].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg1+ang[1]),2))
+        wp_x[2].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg1+ang[2]),2))
+        wp_y[2].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg1+ang[2]),2))
+
+        # add the leader and followers' locations after turning (shall be v = 0)
+        wp_x[0].append(round(Lpx_at,2))
+        wp_y[0].append(round(Lpy_at,2))
+        wp_x[1].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg2+ang[1]),2))
+        wp_y[1].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg2+ang[1]),2))
+        wp_x[2].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg2+ang[2]),2))
+        wp_y[2].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg2+ang[2]),2))
+    
+    # add the decelerating position before ending mission
+    wp_x[0].append(round(x[-1]-dec_v_dist*math.cos(hdg2),2))
+    wp_y[0].append(round(y[-1]-dec_v_dist*math.sin(hdg2),2))
+    wp_x[1].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg2+ang[1]),2))
+    wp_y[1].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg2+ang[1]),2))
+    wp_x[2].append(round(wp_x[0][-1]+Desired_dist*math.cos(math.pi+hdg2+ang[2]),2))
+    wp_y[2].append(round(wp_y[0][-1]+Desired_dist*math.sin(math.pi+hdg2+ang[2]),2))
+
+    # add on the last point
+    wp_x[0].append(x[-1])
+    wp_y[0].append(y[-1])
+    wp_x[1].append(x[-1]+Desired_dist*math.cos(math.pi+hdg2+ang[1]))
+    wp_y[1].append(y[-1]+Desired_dist*math.sin(math.pi+hdg2+ang[1]))
+    wp_x[2].append(x[-1]+Desired_dist*math.cos(math.pi+hdg2+ang[2]))
+    wp_y[2].append(y[-1]+Desired_dist*math.sin(math.pi+hdg2+ang[2]))
+
+    return wp_x, wp_y, Ltang
+    
+    fig = plt.figure()
+    norm = colors.Normalize(vmin=0, vmax=6)
+    plt.plot(np.asarray(x), np.asarray(y), color = cm.hsv(norm(4)))
+    # plt.plot(np.asarray(wp_x[0]), np.asarray(wp_y[0]), color = cm.hsv(norm(1)))
+    # plt.plot(np.asarray(wp_x[1]), np.asarray(wp_y[1]), color = cm.hsv(norm(2)))
+    # plt.plot(np.asarray(wp_x[2]), np.asarray(wp_y[2]), color = cm.hsv(norm(3)))
+    plt.scatter(np.asarray(wp_x[0]), np.asarray(wp_y[0]), color = cm.hsv(norm(1)))
+    plt.scatter(np.asarray(wp_x[1]), np.asarray(wp_y[1]), color = cm.hsv(norm(2)))
+    plt.scatter(np.asarray(wp_x[2]), np.asarray(wp_y[2]), color = cm.hsv(norm(3)))
+    plt.grid()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.axis('equal')
+    plt.show()
+
+def t2pv():
+    pass
+
+def speed_time(Desired_dist, Angle, x, y, v0, a, w0):
+    '''
+    Desired_dist: disired distance between leader and follower (m)
+    Angle: desired angle between leader and follower (deg)
+    x: x coordinates of waypoints (m)
+    y: y coordinates of waypoints (m)
+    v0: preferred speed while going straight with constant speed (m/s)
+    a: preferred acceleration while going straight and decelerating (m^2/s)
+    w0: preferred angular speed while turning (deg/s)
+    '''
+
+    # convert unit (deg2rad)
+    w0 *= 0.01745
+    
+    # desired follower to follower distance
+    Desired_dist_f2f = 2*math.sin(Angle*0.01745)/math.sin(90*0.01745)*Desired_dist
+    
+    # get the uavs positions at three stages (straight line with constant speed, straight line decelerating, and turning)
+    # and the turing angle
+    wp_x, wp_y, Ltang = three_stage_pos(Desired_dist, Angle, x, y, v0, a)
+    
+    # Calculate the time at each wpt each stage 
+    time = [0] 
+    for i in range(1, len(x)-1):
+        # straight line with constant speed
+        time.append(((wp_x[0][(i-1)*3+1] - wp_x[0][(i-1)*3])**2+(wp_y[0][(i-1)*3+1] - wp_y[0][(i-1)*3])**2)**0.5/v0 + time[-1])
+        # straight line decelerating
+        time.append((0-v0)/a + time[-1])
+        # turning
+        time.append(abs(Ltang[i])/w0 + time[-1])
+    # Add last wpt's 
+    time.append(((wp_x[0][-2] - wp_x[0][-3])**2+(wp_y[0][-2] - wp_y[0][-3])**2)**0.5/v0 + time[-1])
+    time.append((0-v0)/a + time[-1])
+    # # total time len = len(wp_x[0]) = 1st pt + 3 stages of intermediate wpt + slow down and last pt = 1 + 3*(wpt-2) + 2
+    # print(len(x), len(wp_x[0]), len(time), time)
+
+    time_no, i = 0, 0 # time no/idex, wpt no/idex
+    norm_x = (x[i+1] - x[i])/((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)**0.5
+    norm_y = (y[i+1] - y[i])/((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)**0.5
+    
+    dt = 0.25
+    t_lin = np.linspace(time[0], time[-1], int((time[-1]-time[0])/dt))
+    
+    vx, vy = {0:[], 1:[], 2:[]}, {0:[], 1:[], 2:[]}
+    vx, vy = {0:[v0*norm_x], 1:[v0*norm_x], 2:[v0*norm_x]}, {0:[v0*norm_y], 1:[v0*norm_y], 2:[v0*norm_y]}
+    px, py = {0:[wp_x[0][0]], 1:[wp_x[1][0]], 2:[wp_x[2][0]]}, {0:[wp_y[0][0]], 1:[wp_y[1][0]], 2:[wp_y[2][0]]}
+    theta0 = {0:0, 1:0, 2:0}
+
+
+    for t in t_lin:
+        if t > time[time_no+1]:
+            time_no += 1
+            i = int(time_no/3)
+            if (time_no - i*3 + 1)%3 == 0: # turning
+                # Get the pivot follower id (compare distance)
+                dx1, dy1 = (wp_x[1][time_no]-wp_x[1][time_no+1]), (wp_y[1][time_no]-wp_y[1][time_no+1])
+                dx2, dy2 = (wp_x[2][time_no]-wp_x[2][time_no+1]), (wp_y[2][time_no]-wp_y[2][time_no+1])
+                if dx1**2 + dy1**2 < dx2**2 + dy2**2:
+                    pivot_id = 1
+                    theta0[2] = math.atan2((wp_y[pivot_id][time_no] - wp_y[2][time_no]),(wp_x[pivot_id][time_no] - wp_x[2][time_no])) + math.pi
+                else: 
+                    pivot_id = 2
+                    theta0[1] = math.atan2((wp_y[pivot_id][time_no] - wp_y[1][time_no]),(wp_x[pivot_id][time_no] - wp_x[1][time_no])) + math.pi
+                theta0[0] = math.atan2((wp_y[pivot_id][time_no] - wp_y[0][time_no]),(wp_x[pivot_id][time_no] - wp_x[0][time_no])) + math.pi
+                dist = {0: Desired_dist, 1: Desired_dist_f2f, 2: Desired_dist_f2f, pivot_id: 0}            
+            else:
+                norm_x = (x[i+1] - x[i])/((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)**0.5
+                norm_y = (y[i+1] - y[i])/((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)**0.5
+
+        if (time_no - i*3 + 1)%3 == 0: # turning
+            for v in range(3):
+                theta = theta0[v] + (w0*math.copysign(1,Ltang[i+1]))*(t-time[time_no])
+                vx[v].append(-dist[v]*(w0*math.copysign(1,Ltang[i+1]))*math.sin(theta))
+                vy[v].append(dist[v]*(w0*math.copysign(1,Ltang[i+1]))*math.cos(theta))
+                px[v].append(dist[v]*math.cos(theta) + wp_x[pivot_id][time_no])
+                py[v].append(dist[v]*math.sin(theta) + wp_y[pivot_id][time_no])
+        else:
+            if (time_no - i*3 + 1)%2 == 0: # decelerating
+                acc = a
+            else: acc = 0 # constant speed
+            for v in range(3):
+                vx[v].append((v0 + acc*(t-time[time_no]))*norm_x)
+                vy[v].append((v0 + acc*(t-time[time_no]))*norm_y)
+                px[v].append(wp_x[v][time_no]+(v0*(t-time[time_no])+0.5*acc*(t-time[time_no])**2)*norm_x)
+                py[v].append(wp_y[v][time_no]+(v0*(t-time[time_no])+0.5*acc*(t-time[time_no])**2)*norm_y)
+    
+
+    fig = plt.figure()
+    norm = colors.Normalize(vmin=0, vmax=6)
+    plt.plot(np.asarray(x), np.asarray(y), color = cm.hsv(norm(4)))
+    plt.plot(np.asarray(px[0]), np.asarray(py[0]), color = cm.hsv(norm(1)))
+    plt.plot(np.asarray(px[1]), np.asarray(py[1]), color = cm.hsv(norm(2)))
+    plt.plot(np.asarray(px[2]), np.asarray(py[2]), color = cm.hsv(norm(3)))
+    # plt.scatter(np.asarray(px[0]), np.asarray(py[0]), color = 'r') #cm.hsv(norm(1))
+    # plt.scatter(np.asarray(px[1]), np.asarray(py[1]), color = cm.hsv(norm(2)))
+    # plt.scatter(np.asarray(px[2]), np.asarray(py[2]), color = cm.hsv(norm(3)))
+    # plt.plot(np.asarray(wp_x[0]), np.asarray(wp_y[0]), color = cm.hsv(norm(1)))
+    # plt.plot(np.asarray(wp_x[1]), np.asarray(wp_y[1]), color = cm.hsv(norm(2)))
+    # plt.plot(np.asarray(wp_x[2]), np.asarray(wp_y[2]), color = cm.hsv(norm(3)))
+    plt.scatter(np.asarray(wp_x[0]), np.asarray(wp_y[0]), color = cm.hsv(norm(1)))
+    plt.scatter(np.asarray(wp_x[1]), np.asarray(wp_y[1]), color = cm.hsv(norm(2)))
+    plt.scatter(np.asarray(wp_x[2]), np.asarray(wp_y[2]), color = cm.hsv(norm(3)))
+    plt.grid()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.axis('equal')
+    plt.show()
+
 # triangle_pi(10, 10, 60, 10, [0, 1, 51, 45], [0, 50, 60, 0])
 # triangle_s(10, 10, 60, 10, [0, 1, 51, 45], [0, 50, 60, 0])
 # triangle_c(10, 10, 60, 10, [0, 1, 51, 45], [0, 50, 60, 0])
 # triangle_c(10, 10, 60, 10, [0, 1, 51, 45], [0, 50, 60, 100])
 # triangle_c(10, 10, 60, 10, [0, 1, 51, 45, -30, -5, 60], [20, 70, 60, 0, 3, -50, -40])
-triangle_c(10, 10, 60, 10, [0, 1, 51, 40, 60, 100, 80, 120], [0, 50, 52, 2, -50, -45, 40, 35])
-
-
+# triangle_c(10, 10, 60, 10, [0, 1, 51, 40, 60, 100, 80, 120], [0, 50, 52, 2, -50, -45, 40, 35])
+# dec_speed(3, -1.8) # 2.5 m
+# three_stage_pos(10, 60, [0, 1, 51, 40, 60, 100, 80, 120], [0, 50, 52, 2, -50, -45, 40, 35], 3, -1.5)
+speed_time(10, 60, [0, 1, 51, 40, 60, 100, 80, 120], [0, 50, 52, 2, -50, -45, 40, 35], 3, -1.5, 30)
+# speed_time(10, 60, [0, 1, 51, 40], [0, 50, 52, 2], 3, -1.5, 60)
